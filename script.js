a1a189b80396fdfac8570975b3bc0cf836c40203
Brian McPhillips <B.McPhillips89@gmail.com>
2021-02-24 14:27:45 -0800

    Display time until new year to DOM

--- script.js
+++ script.js
@@ -10,10 +10,15 @@ const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 function updateCountdown() {
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
-  const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-  const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
-  const minutes = Math.floor(diff / 1000 / 60) % 60;
-  const seconds = Math.floor(diff / 1000) % 60;
+  const daysTime = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const hoursTime = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const minutesTime = Math.floor(diff / 1000 / 60) % 60;
+  const secondsTime = Math.floor(diff / 1000) % 60;
+
+  days.innerHTML = daysTime;
+  hours.innerHTML = hoursTime;
+  minutes.innerHTML = minutesTime;
+  seconds.innerHTML = secondsTime;
 }
 
 setInterval(updateCountdown, 1000);
